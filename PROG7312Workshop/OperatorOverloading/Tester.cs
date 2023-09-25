9d6d6a000d7073c107e255c2b6e6ccc95b6652ee
ktlhape <ktlhape@iie.ac.za>
2023-09-25 08:44:41 +0200

    Implement Operator Overloading to make sorting student objects much more simple😀

--- PROG7312Workshop/OperatorOverloading/Tester.cs
+++ PROG7312Workshop/OperatorOverloading/Tester.cs
@@ -11,24 +11,101 @@ namespace PROG7312Workshop.OperatorOverloading
         static void Main(string[] args)
         {
 
-            Point p1 = new Point(15, 4);
-            Point p2 = new Point(6, 10);
+            //Point p1 = new Point(15, 4);
+            //Point p2 = new Point(6, 10);
 
-            Point p3 = p1 + p2; //11,14
-            Point p4 = p3 + 5;
+            //Point p3 = p1 + p2; //11,14
+            //Point p4 = p3 + 5;
 
-            bool results = p2 > p1;
+            //bool results = p2 > p1;
 
-            Console.WriteLine(p1);
-            Console.WriteLine(p2);
-            Console.WriteLine("===================");
-            Console.WriteLine(p3);
-            Console.WriteLine("=====================");
-            Console.WriteLine(p4);
-            Console.WriteLine(results);
+            //Console.WriteLine(p1);
+            //Console.WriteLine(p2);
+            //Console.WriteLine("===================");
+            //Console.WriteLine(p3);
+            //Console.WriteLine("=====================");
+            //Console.WriteLine(p4);
+            //Console.WriteLine(results);
+            Random rdn = new Random();
 
+            List<Student> stList = new List<Student>() {
+            new Student(rdn.Next(1000,9999),"David","Smith",23),
+            new Student(rdn.Next(1000,9999),"Roger","Federer",22),
+            new Student(rdn.Next(1000,9999),"Michael","Jordan",20),
+            new Student(rdn.Next(1000,9999),"Mika","Federer",23),
+            new Student(rdn.Next(1000,9999),"Jessica","Jones",21),
+            new Student(rdn.Next(1000,9999),"Caroline","Smith",19),
+            new Student(rdn.Next(1000,9999),"Mike","Ross",20),
+            new Student(rdn.Next(1000,9999),"Michael","Smith",19)
+            };
+            DisplayStudents(stList);
+            BubbleSort(stList);
+            DisplayStudents(stList);
 
         }
+        private static void DisplayStudents(List<Student> stList)
+        {
+            foreach (Student st in stList)
+            {
+                Console.WriteLine(st);
+            }
+        }
+        private static void BubbleSort(List<Student> stList)
+        {
+            for (int i = 0; i < stList.Count - 1; i++)
+            {
+                for (int j = (i + 1); j < stList.Count; j++)
+                {
+                    if (stList[i] > stList[j])
+                    {
+                        Student temp = stList[i];
+                        stList[i] = stList[j];
+                        stList[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("==========================");
+        }
+    }
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+
+        public Student(int studentId, string firstname, string lastname, int age)
+        {
+            StudentId = studentId;
+            Firstname = firstname;
+            Lastname = lastname;
+            Age = age;
+        }
+        public static bool operator >(Student s1, Student s2)
+        {
+            int results = string.Compare(s1.Lastname, s2.Lastname);
+            if (results == 0)
+            {
+                results = string.Compare(s1.Firstname, s2.Firstname);
+            }
+            return results > 0;
+        }
+        public static bool operator <(Student s1, Student s2)
+        {
+            int results = string.Compare(s1.Lastname, s2.Lastname);
+            if (results == 0)
+            {
+                results = string.Compare(s1.Firstname, s2.Firstname);
+            }
+            return results < 0;
+        }
+
+        public override string? ToString()
+        {
+            return $"[{StudentId}] {Firstname} {Lastname} - {Age}";
+        }
+
+     
     }
 
     public class Point
